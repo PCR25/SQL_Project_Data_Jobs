f0e1b2d74f35b905b7c6f7bb32fe857b55d26a25
Puscasu Cristian <cristi.robert250@gmail.com>
2025-04-10 12:27:15 +0300

    initial commit

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,14 @@
+
+Select 
+ skills,
+ count(skills_job_dim.job_id) as demand_count
+ from job_postings_fact
+ inner JOIN skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
+inner join skills_dim on skills_job_dim.skill_id = skills_dim.skill_id 
+WHERE
+job_title_short = 'Data Analyst' and job_work_from_home = TRUE
+GROUP BY 
+skills
+ORDER BY
+demand_count desc
+Limit 5 
